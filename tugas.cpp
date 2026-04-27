a8e99f3bfd13f67489befd7b05f0adc45960187e
dfamrln <mhdffa1@gmail.com>
2026-04-27 16:51:23 +0700

    cout hasil luas

--- tugas.cpp
+++ tugas.cpp
@@ -67,5 +67,9 @@ int main() {
     ObjectLayang.inputData();
     ObjectBelah.inputData();
 
+    cout << "\n--- Hasil luas ---" << endl;
+    cout << "Luas Layang-Layang     : " << ObjectLayang.hitungLuas() << endl;
+    cout << "Luas Belah Ketupat     : " << ObjectBelah.hitungLuas() <<endl;
+
    
 }
